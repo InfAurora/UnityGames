0baf2bbbe359bc5e1c87bee37e38cd9a7e0ebaba
Jose Alba <albajo0123@gmail.com>
2020-11-01 01:46:27 -0600

    Created player and enemies that move with spawner

---
+++ TopDown/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed;
+    private Transform playerPosition;
+    // Start is called before the first frame update
+    void Start() {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = Vector2.MoveTowards(transform.position, playerPosition.position, speed * Time.deltaTime);
+    }
+}
---
+++ TopDown/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rigidBody;
+    Vector2 moveVelocity;
+    // Start is called before the first frame update
+    void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate() {
+        rigidBody.MovePosition(rigidBody.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
